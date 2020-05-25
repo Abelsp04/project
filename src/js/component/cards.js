7bc8126cedc2d04e4c8b1821f48b6a8e3b147d99
Abel Segui <55262915+Abelsp04@users.noreply.github.com>
2020-05-24 23:21:29 -0400

    cards change

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -2,53 +2,40 @@ import React from "react";
 
 export function Cards() {
 	return (
-		<div className="row row-cols-1 row-cols-md-2">
-			<div className="col mb-4">
-				<div className="card">
-					<img src="..." className="card-img-top" alt="..." />
+		<div className="card mb-3">
+			<div className="row no-gutters">
+				<div className="col-md-4">
+					<img src="..." className="card-img" alt="..." />
+				</div>
+				<div className="col-md-8">
 					<div className="card-body">
 						<h5 className="card-title">Card title</h5>
 						<p className="card-text">
-							This is a longer card with supporting text below as a natural lead-in to additional content.
+							This is a wider card with supporting text below as a natural lead-in to additional content.
 							This content is a little bit longer.
 						</p>
+						<p className="card-text">
+							<small className="text-muted">Last updated 3 mins ago</small>
+						</p>
 					</div>
 				</div>
-			</div>
-			<div className="col mb-4">
-				<div className="card">
-					<img src="..." className="card-img-top" alt="..." />
+		
+		<div className="col-md-4">
+					<img src="..." className="card-img" alt="..." />
+				</div>
+				<div className="col-md-8">
 					<div className="card-body">
 						<h5 className="card-title">Card title</h5>
 						<p className="card-text">
-							This is a longer card with supporting text below as a natural lead-in to additional content.
+							This is a wider card with supporting text below as a natural lead-in to additional content.
 							This content is a little bit longer.
 						</p>
-					</div>
-				</div>
-			</div>
-			<div className="col mb-4">
-				<div className="card">
-					<img src="..." className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
 						<p className="card-text">
-							This is a longer card with supporting text below as a natural lead-in to additional content.
+							<small className="text-muted">Last updated 3 mins ago</small>
 						</p>
 					</div>
 				</div>
-			</div>
-			<div className="col mb-4">
-				<div className="card">
-					<img src="..." className="card-img-top" alt="..." />
-					<div className="card-body">
-						<h5 className="card-title">Card title</h5>
-						<p className="card-text">
-							This is a longer card with supporting text below as a natural lead-in to additional content.
-							This content is a little bit longer.
-						</p>
-					</div>
-				</div>
+
 			</div>
 		</div>
 	);
